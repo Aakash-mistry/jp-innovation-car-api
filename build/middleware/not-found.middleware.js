6d75c4b4321f506cffaee2394ff1135a75c2ffb6
mistryaksh <111272027+mistryaksh@users.noreply.github.com>
2024-09-19 17:25:41 +0530

    remove vercel

--- build/middleware/not-found.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundMiddleware = void 0;
-const utils_1 = require("../utils");
-const notFoundMiddleware = (_, res) => {
-    return (0, utils_1.NotFound)(res, "API ENDPOINT NOT FOUND");
-};
-exports.notFoundMiddleware = notFoundMiddleware;
--- build/routes/car-sell.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CarSellRoute = void 0;
-const express_1 = require("express");
-const controller_1 = require("../controller");
-exports.CarSellRoute = (0, express_1.Router)();
-exports.CarSellRoute.get("/car-sell", controller_1.CarSellController.getAllSellCars);
-exports.CarSellRoute.post("/car-sell", controller_1.CarSellController.createNewSellCar);
